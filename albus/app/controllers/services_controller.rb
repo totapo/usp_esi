bea3068d899c18459a025d33ea9acab20e38bd10
totapo <matheusraveneziani@gmail.com>
2017-11-24 21:53:47 -0200

    servicoes vitao

--- albus/app/controllers/services_controller.rb
+++ albus/app/controllers/services_controller.rb
@@ -3,14 +3,29 @@ class ServicesController < ApplicationController
   # GET /services/stops
   def stops
     @stops=[]
-    if(params[:routeId])
+    if(params[:lineId])
       #retorna os pontos da rota, na ordem
+      id = Integer(params[:lineId])
+      @stops= Line.find(id).spots
     else
       #retorna todas as paradas da base
       @stops = Spot.all
     end
 
     render json: @stops
+  end
+
+  def lines
+    @lines=[]
+    if(params[:stopId])
+      id = Integer(params[:stopId])
+      @s = Spot.find(id)
+      @lines=@s.lines
+    else
+      @lines = Line.all
+    end
+
+    render json: @lines
   end
 
 
